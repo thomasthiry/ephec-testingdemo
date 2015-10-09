001bbd874ef0799830b96ca0d0b1c5055d696a59
Thomas Thiry <thomas.thiry@ingenico.com>
2015-10-09 22:24:22 +0200

    Cleaned misc display and unused views

--- DAL/Models/Employee.cs
+++ DAL/Models/Employee.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 
 namespace TestingDemo.DAL.Models
@@ -6,9 +7,12 @@ namespace TestingDemo.DAL.Models
     public class Employee
     {
         public int Id { get; set; }
+        [Display(Name = "Last name")]
         public string LastName { get; set; }
+        [Display(Name = "First name")]
         public string FirstName { get; set; }
         [DataType(DataType.Date)]
+        [Display(Name = "Birth date")]
         public DateTime BirthDate { get; set; }
         public decimal Salary { get; set; }
     }
