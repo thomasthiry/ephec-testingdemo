ba069397cc22e86da16f7486a75f2b1bd10fd00e
Thomas Thiry <thomas.thiry@ingenico.com>
2015-10-12 22:10:39 +0200

    Removed YearlyCost from Employee

--- BLL/Employee.cs
+++ BLL/Employee.cs
@@ -5,15 +5,25 @@ namespace TestingDemo.Domain
     public class Employee
     {
         public int Id { get; set; }
+
         public string LastName { get; set; }
+
         public string FirstName { get; set; }
+
         public DateTime BirthDate { get; set; }
+
         public decimal Salary { get; set; }
+
         public bool IsMarried { get; set; }
+
         public int NumberOfChildren { get; set; }
+
         public bool HasMealVouchers { get; set; }
+
         public decimal HealthInsuranceMonthlyAmount { get; set; }
+
         public decimal GroupInsuranceMonthlyAmount { get; set; }
+
         public decimal CellphoneMonthlyAmount { get; set; }
 
         public decimal MonthlyCost
@@ -28,13 +38,6 @@ namespace TestingDemo.Domain
 
                 return Salary + mealVoucherMonthlyAmount + HealthInsuranceMonthlyAmount + GroupInsuranceMonthlyAmount + CellphoneMonthlyAmount;
             }
-        }
-
-        public decimal YearlyCost
-        {
-            // double pécule de vacance = 92 % en +
-
-            get { return MonthlyCost*12; }
         }
     }
 }
--- TestingDemo.Tests/Controllers/HomeControllerTest.cs
+++ TestingDemo.Tests/Controllers/HomeControllerTest.cs
@@ -2,6 +2,25 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using TestingDemo.Controllers;
 
+
+//Tests:
+//- MonthlyCost_OnlySalary_ReturnsSalary
+//- MonthlyCost_OnlyMealVouchers_ReturnsSumForMonth
+//- MonthlyCost_FullPackage_ReturnsFullTotal
+
+//TDD:
+//- YearlyCost_FullPackage_ReturnsYearlyCost
+//- MonthlyCost_FullPackage_IncludesHolidayBonus
+//- MonthlyCost_Maried_IncludesPartnerFee
+//- MonthlyCost_Family_IncludesFamilyFee
+
+//BDD:
+//- DailyCost_FullPackage
+
+//UI:
+//- CreateEmployee
+
+
 namespace TestingDemo.Domain.Tests.Controllers
 {
     [TestClass]
