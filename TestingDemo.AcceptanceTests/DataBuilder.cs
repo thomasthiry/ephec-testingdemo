403907a938eddcb360c5f0d05bae7ee2d898034f
Thomas Thiry <thomas.thiry@ingenico.com>
2015-11-14 21:36:20 +0100

    Updated Acc tests and done 1st demo 2

---
+++ TestingDemo.AcceptanceTests/DataBuilder.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestingDemo.Infrastructure;
+using TestingDemo.Infrastructure.Models;
+
+namespace TestingDemo.AcceptanceTests
+{
+    class DataBuilder
+    {
+        private TestingDemoContext dbContext = new TestingDemoContext();
+
+        public void AddEmployeeInDb(string lastName, string firstname)
+        {
+            var employee = new Employee
+            {
+                FirstName = firstname,
+                LastName = lastName,
+                BirthDate = DateTime.Today.AddYears(-25)
+            };
+
+            dbContext.Employees.Add(employee);
+            dbContext.SaveChanges();
+        }
+    }
+}
